877f0fb51d3effd62fdca7d0015bf849dfa84587
Jens Wiklander <jens.wiklander@linaro.org>
2016-01-28 18:35:11 +0100

    qemu: generic driver
    Build support for generic driver on QEMU.
    
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- qemu.mk
+++ qemu.mk
@@ -32,8 +32,7 @@ DEBUG = 1
 ################################################################################
 all: bios-qemu qemu soc-term
 all-clean: bios-qemu-clean busybox-clean linux-clean optee-os-clean \
-	optee-client-clean optee-linuxdriver-clean qemu-clean soc-term-clean \
-	check-clean
+	optee-client-clean qemu-clean soc-term-clean check-clean
 
 -include toolchain.mk
 
@@ -113,12 +112,6 @@ optee-client: optee-client-common
 
 optee-client-clean: optee-client-clean-common
 
-OPTEE_LINUXDRIVER_COMMON_FLAGS += ARCH=arm
-optee-linuxdriver: optee-linuxdriver-common
-
-OPTEE_LINUXDRIVER_CLEAN_COMMON_FLAGS += ARCH=arm
-optee-linuxdriver-clean: optee-linuxdriver-clean-common
-
 ################################################################################
 # Soc-term
 ################################################################################
@@ -157,8 +150,7 @@ filelist-tee: xtest
 	@echo "# OP-TEE device" >> $(GEN_ROOTFS_FILELIST)
 	@echo "dir /lib/modules 755 0 0" >> $(GEN_ROOTFS_FILELIST)
 	@echo "dir /lib/modules/$(call KERNEL_VERSION) 755 0 0" >> $(GEN_ROOTFS_FILELIST)
-	@echo "file /lib/modules/$(call KERNEL_VERSION)/optee.ko $(OPTEE_LINUXDRIVER_PATH)/core/optee.ko 755 0 0" >> $(GEN_ROOTFS_FILELIST)
-	@echo "file /lib/modules/$(call KERNEL_VERSION)/optee_armtz.ko $(OPTEE_LINUXDRIVER_PATH)/armtz/optee_armtz.ko 755 0 0" >> $(GEN_ROOTFS_FILELIST)
+	@echo "file /lib/modules/$(call KERNEL_VERSION)/optee.ko $(LINUX_PATH)/drivers/tee/optee.ko 755 0 0" >> $(GEN_ROOTFS_FILELIST)
 	@echo "# OP-TEE Client" >> $(GEN_ROOTFS_FILELIST)
 	@echo "file /bin/tee-supplicant $(OPTEE_CLIENT_EXPORT)/bin/tee-supplicant 755 0 0" >> $(GEN_ROOTFS_FILELIST)
 	@echo "dir /lib/arm-linux-gnueabihf 755 0 0" >> $(GEN_ROOTFS_FILELIST)
@@ -166,7 +158,7 @@ filelist-tee: xtest
 	@echo "slink /lib/arm-linux-gnueabihf/libteec.so.1 libteec.so.1.0 755 0 0" >> $(GEN_ROOTFS_FILELIST)
 	@echo "slink /lib/arm-linux-gnueabihf/libteec.so libteec.so.1 755 0 0" >> $(GEN_ROOTFS_FILELIST)
 
-update_rootfs: busybox optee-client optee-linuxdriver filelist-tee
+update_rootfs: busybox optee-client filelist-tee
 	cat $(GEN_ROOTFS_PATH)/filelist-final.txt $(GEN_ROOTFS_PATH)/filelist-tee.txt > $(GEN_ROOTFS_PATH)/filelist.tmp
 	cd $(GEN_ROOTFS_PATH); \
 		$(LINUX_PATH)/usr/gen_init_cpio $(GEN_ROOTFS_PATH)/filelist.tmp | gzip > $(GEN_ROOTFS_PATH)/filesystem.cpio.gz
