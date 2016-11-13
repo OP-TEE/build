OP-TEE/build

commit 97d0529c1687e9a053e33e2e25c832347f550da0

Enable configuring QEMU with Virtfs support
Add support for configuring QEMU with VirtFS to share data
between the guest QEMU VM and host operating systems.

To specify dir, that has to be mounted inside QEMU VM, edit
QEMU_VIRTFS_HOST_DIR var in qemu.mk

To mount shared folder within QEMU ARM VM, run:
$ mount -t 9p -o trans=virtio host /tmp/shared/

If Virtfs support is enabled, libattr1-dev and libcap-dev
have to be installed before configuring QEMU.

For additional details see:
http://wiki.qemu.org/Documentation/9psetup

Signed-off-by: Igor Opaniuk <igor.opaniuk@linaro.org>
Reviewed-by: Joakim Bech <joakim.bech@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Tested-by: Igor Opaniuk <igor.opaniuk@linaro.org> (qemu_v7/v8)

common.mk
@@ -22,6 +22,12 @@ CFG_TEE_CORE_LOG_LEVEL		?= 3
 
 CCACHE ?= $(shell which ccache) # Don't remove this comment (space is needed)
 
+# Accessing a shared folder on the host from QEMU:
+# # Set QEMU_VIRTFS_ENABLE to 'y' and adjust QEMU_VIRTFS_HOST_DIR
+# # Then in QEMU, run:
+# # $ mount -t 9p -o trans=virtio host <mount_point>
+QEMU_VIRTFS_ENABLE		?= n
+QEMU_VIRTFS_HOST_DIR	?= $(ROOT)
 
 ################################################################################
 # Check coherency of compilation mode
@@ -179,7 +185,17 @@ edk2-clean-common:
 	$(call edk2-call) clean && \
 	$(MAKE) -j1 -C $(EDK2_PATH)/BaseTools clean && \
 	rm -f $(EDK2_PATH)/Conf/target.txt
+################################################################################
+# QEMU / QEMUv8
+################################################################################
+QEMU_CONFIGURE_PARAMS_COMMON = --cc="$(CCACHE)gcc" --extra-cflags="-Wno-error"
 
+ifeq ($(QEMU_VIRTFS_ENABLE),y)
+QEMU_CONFIGURE_PARAMS +=  --enable-virtfs
+QEMU_EXTRA_ARGS +=\
+	-fsdev local,id=fsdev0,path=$(QEMU_VIRTFS_HOST_DIR),security_model=none \
+	-device virtio-9p-device,fsdev=fsdev0,mount_tag=host
+endif
 ################################################################################
 # OP-TEE
 ################################################################################

kconfigs/qemu.conf
@@ -4,3 +4,8 @@ CONFIG_OPTEE=y
 ### "make check" in Travis CI
 CONFIG_PREEMPT=y
 CONFIG_JFFS2_FS=n
+### Enable 9P VFS
+CONFIG_NET_9P=y
+CONFIG_NET_9P_VIRTIO=y
+CONFIG_9P_FS=y
+CONFIG_9P_FS_POSIX_ACL=y

qemu.mk
@@ -49,7 +49,8 @@ bios-qemu-clean:
 	$(call bios-qemu-common) clean
 
 qemu:
-	cd $(QEMU_PATH); ./configure --target-list=arm-softmmu --cc="$(CCACHE)gcc" --extra-cflags="-Wno-error"
+	cd $(QEMU_PATH); ./configure --target-list=arm-softmmu\
+			$(QEMU_CONFIGURE_PARAMS_COMMON)
 	$(MAKE) -C $(QEMU_PATH)
 
 qemu-clean:
@@ -179,7 +180,8 @@ run-only:
 		-serial tcp:localhost:54320 -serial tcp:localhost:54321 \
 		-s -S -machine virt -machine secure=on -cpu cortex-a15 \
 		-m 1057 \
-		-bios $(ROOT)/out/bios-qemu/bios.bin $(QEMU_EXTRA_ARGS)
+		-bios $(ROOT)/out/bios-qemu/bios.bin \
+		$(QEMU_EXTRA_ARGS)
 
 
 ifneq ($(filter check,$(MAKECMDGOALS)),)

qemu_v8.mk
@@ -75,7 +75,8 @@ endif
 # QEMU
 ################################################################################
 qemu:
-	cd $(QEMU_PATH); ./configure --target-list=aarch64-softmmu --cc="$(CCACHE)gcc" --extra-cflags="-Wno-error"
+	cd $(QEMU_PATH); ./configure --target-list=aarch64-softmmu\
+			$(QEMU_CONFIGURE_PARAMS_COMMON)
 	$(MAKE) -C $(QEMU_PATH)
 
 qemu-clean:
@@ -231,7 +232,8 @@ run-only:
 		-semihosting -d unimp \
 		-initrd $(GEN_ROOTFS_PATH)/filesystem.cpio.gz \
 		-kernel $(LINUX_PATH)/arch/arm64/boot/Image \
-		-append 'console=ttyAMA0,38400 keep_bootcon root=/dev/vda2'
+		-append 'console=ttyAMA0,38400 keep_bootcon root=/dev/vda2' \
+		$(QEMU_EXTRA_ARGS)
 
 ifneq ($(filter check,$(MAKECMDGOALS)),)
 CHECK_DEPS := all

commit 23b41319e8fef64f790d69797eeaf0dc797db011

qemu: virtfs: fix wrong variable name
"make QEMU_VIRTFS_ENABLE=y" does not enable VirtFS because of a bad
variable name in common.mk.

Test case:

(host) echo Hello >/tmp/hello
(host) make -j8 run QEMU_VIRTFS_ENABLE=y QEMU_VIRTFS_HOST_DIR=/tmp
(QEMU) mkdir /tmp/host
(QEMU) mount_shared /tmp/host
(QEMU) cat /tmp/host/hello
Hello

Fixes: 97d0529c1687 ("Enable configuring QEMU with Virtfs support")
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Igor Opaniuk <igor.opaniuk@linaro.org>

common.mk
@@ -191,7 +191,7 @@ edk2-clean-common:
 QEMU_CONFIGURE_PARAMS_COMMON = --cc="$(CCACHE)gcc" --extra-cflags="-Wno-error"
 
 ifeq ($(QEMU_VIRTFS_ENABLE),y)
-QEMU_CONFIGURE_PARAMS +=  --enable-virtfs
+QEMU_CONFIGURE_PARAMS_COMMON +=  --enable-virtfs
 QEMU_EXTRA_ARGS +=\
 	-fsdev local,id=fsdev0,path=$(QEMU_VIRTFS_HOST_DIR),security_model=none \
 	-device virtio-9p-device,fsdev=fsdev0,mount_tag=host