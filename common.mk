OP-TEE/build

commit 27edfc78b588e742a425ac92da9f8ac5647b7f97

OP-TEE Benchmark
Enable Benchmark feature

Reviewed-by: Joakim Bech <joakim.bech@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Signed-off-by: Igor Opaniuk <igor.opaniuk@linaro.org>

common.mk
@@ -15,11 +15,15 @@ OPTEE_OS_PATH			?= $(ROOT)/optee_os
 OPTEE_CLIENT_PATH		?= $(ROOT)/optee_client
 OPTEE_CLIENT_EXPORT		?= $(OPTEE_CLIENT_PATH)/out/export
 OPTEE_TEST_PATH			?= $(ROOT)/optee_test
-OPTEE_TEST_OUT_PATH 		?= $(ROOT)/optee_test/out
+OPTEE_TEST_OUT_PATH		?= $(ROOT)/optee_test/out
 HELLOWORLD_PATH			?= $(ROOT)/hello_world
+BENCHMARK_APP_PATH		?= $(ROOT)/optee_benchmark
 
 # default high verbosity. slow uarts shall specify lower if prefered
 CFG_TEE_CORE_LOG_LEVEL		?= 3
+
+# default disable latency benchmarks (over all OP-TEE layers)
+CFG_TEE_BENCHMARK			?= n
 
 CCACHE ?= $(shell which ccache) # Don't remove this comment (space is needed)
 
@@ -145,6 +149,10 @@ busybox-cleaner-common:
 ################################################################################
 # Linux
 ################################################################################
+ifeq ($(CFG_TEE_BENCHMARK),y)
+LINUX_DEFCONFIG_BENCH ?= $(CURDIR)/kconfigs/tee_bench.conf
+endif
+
 LINUX_COMMON_FLAGS ?= LOCALVERSION= CROSS_COMPILE=$(CROSS_COMPILE_NS_KERNEL)
 
 linux-common: linux-defconfig
@@ -153,7 +161,8 @@ linux-common: linux-defconfig
 $(LINUX_PATH)/.config: $(LINUX_DEFCONFIG_COMMON_FILES)
 	cd $(LINUX_PATH) && \
 		ARCH=$(LINUX_DEFCONFIG_COMMON_ARCH) \
-		scripts/kconfig/merge_config.sh $(LINUX_DEFCONFIG_COMMON_FILES)
+		scripts/kconfig/merge_config.sh $(LINUX_DEFCONFIG_COMMON_FILES) \
+			$(LINUX_DEFCONFIG_BENCH)
 
 linux-defconfig-clean-common:
 	rm -f $(LINUX_PATH)/.config
@@ -250,17 +259,19 @@ OPTEE_OS_COMMON_FLAGS ?= \
 	CROSS_COMPILE_ta_arm64=$(AARCH64_CROSS_COMPILE) \
 	CROSS_COMPILE_ta_arm32=$(AARCH32_CROSS_COMPILE) \
 	CFG_TEE_CORE_LOG_LEVEL=$(CFG_TEE_CORE_LOG_LEVEL) \
-	DEBUG=$(DEBUG)
+	DEBUG=$(DEBUG) \
+	CFG_TEE_BENCHMARK=$(CFG_TEE_BENCHMARK)
 
 optee-os-common:
 	$(MAKE) -C $(OPTEE_OS_PATH) $(OPTEE_OS_COMMON_FLAGS)
 
 OPTEE_OS_CLEAN_COMMON_FLAGS ?= $(OPTEE_OS_COMMON_EXTRA_FLAGS)
 
-optee-os-clean-common: xtest-clean helloworld-clean
+optee-os-clean-common: xtest-clean helloworld-clean benchmark-app-clean-common
 	$(MAKE) -C $(OPTEE_OS_PATH) $(OPTEE_OS_CLEAN_COMMON_FLAGS) clean
 
-OPTEE_CLIENT_COMMON_FLAGS ?= CROSS_COMPILE=$(CROSS_COMPILE_NS_USER)
+OPTEE_CLIENT_COMMON_FLAGS ?= CROSS_COMPILE=$(CROSS_COMPILE_NS_USER) \
+	CFG_TEE_BENCHMARK=$(CFG_TEE_BENCHMARK) \
 
 optee-client-common:
 	$(MAKE) -C $(OPTEE_CLIENT_PATH) $(OPTEE_CLIENT_COMMON_FLAGS)
@@ -280,7 +291,8 @@ XTEST_COMMON_FLAGS ?= CROSS_COMPILE_HOST=$(CROSS_COMPILE_NS_USER)\
 	TA_DEV_KIT_DIR=$(OPTEE_OS_TA_DEV_KIT_DIR) \
 	OPTEE_CLIENT_EXPORT=$(OPTEE_CLIENT_EXPORT) \
 	COMPILE_NS_USER=$(COMPILE_NS_USER) \
-	O=$(OPTEE_TEST_OUT_PATH)
+	O=$(OPTEE_TEST_OUT_PATH) \
+	CFG_TEE_BENCHMARK=$(CFG_TEE_BENCHMARK)
 
 xtest-common: optee-os optee-client
 	$(MAKE) -C $(OPTEE_TEST_PATH) $(XTEST_COMMON_FLAGS)
@@ -313,6 +325,19 @@ helloworld-clean-common:
 	$(MAKE) -C $(HELLOWORLD_PATH) $(HELLOWORLD_CLEAN_COMMON_FLAGS) clean
 
 ################################################################################
+# benchmark_app
+################################################################################
+BENCHMARK_APP_COMMON_FLAGS ?= HOST_CROSS_COMPILE=$(CROSS_COMPILE_NS_USER) \
+	TEEC_EXPORT=$(OPTEE_CLIENT_EXPORT) \
+	TEEC_INTERNAL_INCLUDES=$(OPTEE_CLIENT_PATH)/libteec
+
+benchmark-app-common: optee-os optee-client
+	$(MAKE) -C $(BENCHMARK_APP_PATH) $(BENCHMARK_APP_COMMON_FLAGS)
+
+benchmark-app-clean-common:
+	$(MAKE) -C $(BENCHMARK_APP_PATH) clean
+
+################################################################################
 # rootfs
 ################################################################################
 update_rootfs-common: busybox filelist-tee
@@ -329,7 +354,7 @@ update_rootfs-clean-common:
 	rm -f $(GEN_ROOTFS_FILELIST)
 
 filelist-tee-common: fl:=$(GEN_ROOTFS_FILELIST)
-filelist-tee-common: optee-client xtest helloworld
+filelist-tee-common: optee-client xtest helloworld benchmark-app
 	@echo "# filelist-tee-common /start" 				> $(fl)
 	@echo "dir /lib/optee_armtz 755 0 0" 				>> $(fl)
 	@echo "# xtest / optee_test" 					>> $(fl)
@@ -344,6 +369,10 @@ filelist-tee-common: optee-client xtest helloworld
 		echo "file /lib/optee_armtz/8aaaf200-2450-11e4-abe2-0002a5d5c51b.ta" \
 			"$(HELLOWORLD_PATH)/ta/8aaaf200-2450-11e4-abe2-0002a5d5c51b.ta" \
 			"444 0 0" 					>> $(fl); \
+	fi
+	@if [ -e $(BENCHMARK_APP_PATH)/benchmark ]; then \
+		echo "file /bin/benchmark" \
+			"$(BENCHMARK_APP_PATH)/benchmark 755 0 0"	>> $(fl); \
 	fi
 	@if [ "$(QEMU_USERNET_ENABLE)" = "y" ]; then \
 		echo "slink /etc/rc.d/S02_udhcp_networking /etc/init.d/udhcpc 755 0 0" \

qemu.mk
@@ -23,9 +23,9 @@ DEBUG = 1
 ################################################################################
 # Targets
 ################################################################################
-all: bios-qemu qemu soc-term
+all: bios-qemu qemu soc-term benchmark-app
 clean: bios-qemu-clean busybox-clean linux-clean optee-os-clean \
-	optee-client-clean qemu-clean soc-term-clean check-clean
+	optee-client-clean qemu-clean soc-term-clean check-clean benchmark-app-clean
 
 -include toolchain.mk
 
@@ -129,6 +129,13 @@ xtest-patch: xtest-patch-common
 helloworld: helloworld-common
 
 helloworld-clean: helloworld-clean-common
+
+################################################################################
+# benchmark
+################################################################################
+benchmark-app: benchmark-app-common
+
+benchmark-app-clean: benchmark-app-clean-common
 
 ################################################################################
 # Root FS

kconfigs/tee_bench.conf
@@ -1,0 +1,1 @@
+CONFIG_OPTEE_BENCHMARK=y

commit 4f667cd26dd6172eb02379d1f0d51e16304d18ac

benchmark: fix build
Fixes the following error [1]:

make -C /home/travis/default/build/../optee_benchmark HOST_CROSS_COMPILE="/usr/bin/ccache /home/travis/default/build/../toolchains/aarch32/bin/arm-linux-gnueabihf-" TEEC_EXPORT=/home/travis/default/build/../optee_client/out/export TEEC_INTERNAL_INCLUDES=/home/travis/default/build/../optee_client/libteec
make: *** /home/travis/default/build/../optee_benchmark: No such file or directory.  Stop.

[1] https://travis-ci.org/OP-TEE/build/builds/237729031 job 216.1

Fixes: 27edfc78b588e ("OP-TEE Benchmark")
Signed-off-by: Igor Opaniuk <igor.opaniuk@linaro.org>
Tested-by: Jerome Forissier <jerome.forissier@linaro.org. (HiKey)
[jf: update commit description]
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>

common.mk
@@ -267,7 +267,10 @@ optee-os-common:
 
 OPTEE_OS_CLEAN_COMMON_FLAGS ?= $(OPTEE_OS_COMMON_EXTRA_FLAGS)
 
-optee-os-clean-common: xtest-clean helloworld-clean benchmark-app-clean-common
+ifeq ($(CFG_TEE_BENCHMARK),y)
+optee-os-clean-common: benchmark-app-clean-common
+endif
+optee-os-clean-common: xtest-clean helloworld-clean
 	$(MAKE) -C $(OPTEE_OS_PATH) $(OPTEE_OS_CLEAN_COMMON_FLAGS) clean
 
 OPTEE_CLIENT_COMMON_FLAGS ?= CROSS_COMPILE=$(CROSS_COMPILE_NS_USER) \
@@ -291,8 +294,7 @@ XTEST_COMMON_FLAGS ?= CROSS_COMPILE_HOST=$(CROSS_COMPILE_NS_USER)\
 	TA_DEV_KIT_DIR=$(OPTEE_OS_TA_DEV_KIT_DIR) \
 	OPTEE_CLIENT_EXPORT=$(OPTEE_CLIENT_EXPORT) \
 	COMPILE_NS_USER=$(COMPILE_NS_USER) \
-	O=$(OPTEE_TEST_OUT_PATH) \
-	CFG_TEE_BENCHMARK=$(CFG_TEE_BENCHMARK)
+	O=$(OPTEE_TEST_OUT_PATH)
 
 xtest-common: optee-os optee-client
 	$(MAKE) -C $(OPTEE_TEST_PATH) $(XTEST_COMMON_FLAGS)
@@ -353,8 +355,11 @@ update_rootfs-clean-common:
 	rm -f $(GEN_ROOTFS_PATH)/filelist-tmp.txt
 	rm -f $(GEN_ROOTFS_FILELIST)
 
+ifeq ($(CFG_TEE_BENCHMARK),y)
+filelist-tee-common: benchmark-app
+endif
 filelist-tee-common: fl:=$(GEN_ROOTFS_FILELIST)
-filelist-tee-common: optee-client xtest helloworld benchmark-app
+filelist-tee-common: optee-client xtest helloworld
 	@echo "# filelist-tee-common /start" 				> $(fl)
 	@echo "dir /lib/optee_armtz 755 0 0" 				>> $(fl)
 	@echo "# xtest / optee_test" 					>> $(fl)

qemu.mk
@@ -23,9 +23,13 @@ DEBUG = 1
 ################################################################################
 # Targets
 ################################################################################
-all: bios-qemu qemu soc-term benchmark-app
+ifeq ($(CFG_TEE_BENCHMARK),y)
+all: benchmark-app
+clean: benchmark-app-clean
+endif
+all: bios-qemu qemu soc-term
 clean: bios-qemu-clean busybox-clean linux-clean optee-os-clean \
-	optee-client-clean qemu-clean soc-term-clean check-clean benchmark-app-clean
+	optee-client-clean qemu-clean soc-term-clean check-clean
 
 -include toolchain.mk
 