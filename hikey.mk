0023a909678ad8c4ec69b7e86b353f81691fb914
Victor Chong <victor.chong@linaro.org>
2017-09-29 18:25:37 +0100

    hikey*: Use l-loader makefile
    l-loader manual build instructions have been integrated into makefiles
    
    Signed-off-by: Victor Chong <victor.chong@linaro.org>
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

--- hikey.mk
+++ hikey.mk
@@ -363,35 +363,16 @@ atf-fb-clean:
 ################################################################################
 # l-loader
 ################################################################################
-.PHONY: lloader-bin
-lloader-bin: arm-tf atf-fb
+.PHONY: lloader
+lloader:
 	cd $(LLOADER_PATH) && \
 		ln -sf $(ARM_TF_PATH)/build/hikey/$(ARM_TF_BUILD)/bl1.bin && \
 		ln -sf $(ATF_FB_PATH)/build/hikey/$(ATF_FB_BUILD)/bl1.bin fastboot.bin && \
-		$(AARCH32_CROSS_COMPILE)gcc -c -o start.o start.S && \
-		$(AARCH32_CROSS_COMPILE)ld -Bstatic -Tl-loader.lds -Ttext 0xf9800800 start.o -o loader && \
-		$(AARCH32_CROSS_COMPILE)objcopy -O binary loader temp && \
-		python gen_loader_hikey.py -o l-loader.bin --img_loader=temp --img_bl1=bl1.bin --img_ns_bl1u=fastboot.bin
-
-.PHONY: lloader-bin-clean
-lloader-bin-clean:
-	cd $(LLOADER_PATH) && \
-		rm -f l-loader.bin temp loader start.o
-
-.PHONY: lloader-ptbl
-lloader-ptbl:
-	cd $(LLOADER_PATH) && \
-		PTABLE=linux-$(CFG_FLASH_SIZE)g SECTOR_SIZE=512 bash -x generate_ptable.sh
-
-.PHONY: lloader-ptbl-clean
-lloader-ptbl-clean:
-	cd $(LLOADER_PATH) && rm -f prm_ptable.img sec_ptable.img
-
-.PHONY: lloader
-lloader: lloader-bin lloader-ptbl
+		$(MAKE) hikey PTABLE_LST=linux-$(CFG_FLASH_SIZE)g CROSS_COMPILE="$(CCACHE)$(AARCH32_CROSS_COMPILE)"
 
 .PHONY: lloader-clean
-lloader-clean: lloader-bin-clean lloader-ptbl-clean
+lloader-clean:
+	$(MAKE) -C $(LLOADER_PATH) hikey-clean
 
 ################################################################################
 # nvme image
