a5941c05d1d7655a2017787ff77c851fcca132e6
Jens Wiklander <jens.wiklander@linaro.org>
2017-10-09 09:56:28 +0200

    qemu_v8: drop -O0 flag for ARM-TF
    Drops the -O0 flag passed in CFLAGS when compiling ARM-TF. This is
    needed to ensure that the binaries doesn't overflow their assigned
    memory.
    
    Acked-by: Jerome Forissier <jerome.forissier@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- qemu_v8.mk
+++ qemu_v8.mk
@@ -43,7 +43,6 @@ clean: arm-tf-clean busybox-clean edk2-clean linux-clean \
 # ARM Trusted Firmware
 ################################################################################
 ARM_TF_EXPORTS ?= \
-	CFLAGS="-O0 -gdwarf-2" \
 	CROSS_COMPILE="$(CCACHE)$(AARCH64_CROSS_COMPILE)"
 
 ARM_TF_FLAGS ?= \
