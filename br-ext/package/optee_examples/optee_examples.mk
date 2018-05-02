84bf6e1fa4e5cbbafda59f6cb7148b0d401b5e73
Jens Wiklander <jens.wiklander@linaro.org>
2018-05-02 16:55:17 +0200

    br-ext: optee_examples: print TA being built
    Reviewed-by: Joakim Bech <joakim.bech@linaro.org>
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- br-ext/package/optee_examples/optee_examples.mk
+++ br-ext/package/optee_examples/optee_examples.mk
@@ -9,8 +9,9 @@ OPTEE_EXAMPLES_CONF_OPTS = -DOPTEE_EXAMPLES_SDK=$(OPTEE_EXAMPLES_SDK)
 
 define OPTEE_EXAMPLES_BUILD_TAS
 	@$(foreach f,$(wildcard $(@D)/*/ta/Makefile), \
-		$(MAKE) CROSS_COMPILE="$(shell echo $(BR2_PACKAGE_OPTEE_EXAMPLES_CROSS_COMPILE))" \
-			O=out TA_DEV_KIT_DIR=$(OPTEE_EXAMPLES_SDK) V=1 \
+		echo Building $f && \
+			$(MAKE) CROSS_COMPILE="$(shell echo $(BR2_PACKAGE_OPTEE_EXAMPLES_CROSS_COMPILE))" \
+			O=out TA_DEV_KIT_DIR=$(OPTEE_EXAMPLES_SDK) \
 			$(TARGET_CONFIGURE_OPTS) -C $(dir $f) all &&) true
 endef
 
