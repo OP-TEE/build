6a0fc2b313599eea876573d85e92b08e82b43069
Joakim Bech <joakim.bech@linaro.org>
2018-05-23 08:38:47 +0200

    toolchain: Remove legacy toolchains
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>
    Tested-by: Joakim Bech <joakim.bech@linaro.org> (QEMUv8, FVP, RPi3)
    Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

--- toolchain.mk
+++ toolchain.mk
@@ -14,19 +14,6 @@ AARCH64_CROSS_COMPILE 		?= $(AARCH64_PATH)/bin/aarch64-linux-gnu-
 AARCH64_GCC_VERSION 		?= gcc-linaro-6.2.1-2016.11-x86_64_aarch64-linux-gnu
 SRC_AARCH64_GCC 		?= https://releases.linaro.org/components/toolchain/binaries/6.2-2016.11/aarch64-linux-gnu/${AARCH64_GCC_VERSION}.tar.xz
 
-# Due to relocation error on the 96board edk forest, let's keep the old
-# toolchain for a while.
-LEGACY_AARCH64_PATH             ?= $(TOOLCHAIN_ROOT)/aarch64-legacy
-LEGACY_AARCH64_CROSS_COMPILE    ?= $(LEGACY_AARCH64_PATH)/bin/aarch64-linux-gnu-
-LEGACY_AARCH64_GCC_VERSION      ?= gcc-linaro-aarch64-linux-gnu-4.9-2014.08_linux
-LEGACY_SRC_AARCH64_GCC          ?= https://releases.linaro.org/archive/14.08/components/toolchain/binaries/${LEGACY_AARCH64_GCC_VERSION}.tar.xz
-
-LEGACY_AARCH32_PATH             ?= $(TOOLCHAIN_ROOT)/aarch32-legacy
-LEGACY_AARCH32_CROSS_COMPILE    ?= $(LEGACY_AARCH32_PATH)/bin/arm-linux-gnueabihf-
-LEGACY_AARCH32_GCC_VERSION      ?= gcc-linaro-4.9-2015.02-3-x86_64_arm-linux-gnueabihf
-LEGACY_SRC_AARCH32_GCC          ?= https://releases.linaro.org/archive/15.02/components/toolchain/binaries/arm-linux-gnueabihf/${LEGACY_AARCH32_GCC_VERSION}.tar.xz
-
-
 # Download toolchain macro for saving some repetition
 # $(1) is $AARCH.._PATH		: i.e., path to the destination
 # $(2) is $SRC_AARCH.._GCC	: is the downloaded tar.gz file
@@ -41,7 +28,7 @@ define dltc
 endef
 
 .PHONY: toolchains
-toolchains: aarch32 aarch64 aarch64-legacy aarch32-legacy
+toolchains: aarch32 aarch64
 
 .PHONY: aarch32
 aarch32:
@@ -50,12 +37,3 @@ aarch32:
 .PHONY: aarch64
 aarch64:
 	$(call dltc,$(AARCH64_PATH),$(SRC_AARCH64_GCC),$(AARCH64_GCC_VERSION))
-
-.PHONY: aarch64-legacy
-aarch64-legacy:
-	$(call dltc,$(LEGACY_AARCH64_PATH),$(LEGACY_SRC_AARCH64_GCC),$(LEGACY_AARCH64_GCC_VERSION))
-
-.PHONY: aarch32-legacy
-aarch32-legacy:
-	$(call dltc,$(LEGACY_AARCH32_PATH),$(LEGACY_SRC_AARCH32_GCC),$(LEGACY_AARCH32_GCC_VERSION))
-
