OP-TEE/build

commit 751b35bb0b84dc33e036d42db69be56c58f971c2

qemu: buildroot: fix networking
Adds the missing startup file to the buildroot-generated filesystem so
that networking is available in the VM as claimed by docs/qemu.md.

Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Reviewed-by: Joakim Bech <joakim.bech@linaro.org>

common.mk
@@ -201,6 +201,8 @@ buildroot: optee-os
 	@touch ../out-br/extra.conf
 	@echo "BR2_TARGET_GENERIC_GETTY_PORT=\"$(BUILDROOT_GETTY_PORT)\"" >> \
 		../out-br/extra.conf
+	@echo "BR2_ROOTFS_OVERLAY=\"$(ROOT)/build/br-ext/board/qemu/overlay\"" >> \
+		../out-br/extra.conf
 	@echo "BR2_PACKAGE_OPTEE_TEST_CROSS_COMPILE=\"$(CROSS_COMPILE_S_USER)\"" >> \
 		../out-br/extra.conf
 	@echo "BR2_PACKAGE_OPTEE_EXAMPLES_CROSS_COMPILE=\"$(CROSS_COMPILE_S_USER)\"" >> \

br-ext/board/qemu/overlay/etc/init.d/S50udhcpc
@@ -1,0 +1,23 @@
+#!/bin/sh
+case "$1" in
+  start)
+	printf "Starting network (udhcpc): "
+	udhcpc >> /var/log/udhcpc.log 2>&1
+	[ $? = 0 ] && echo "OK" || echo "FAIL"
+	;;
+  stop)
+	printf "Stopping network (udhcpc): "
+	killall udhcpc
+	[ $? = 0 ] && echo "OK" || echo "FAIL"
+	;;
+  restart|reload)
+	"$0" stop
+	"$0" start
+	;;
+  *)
+	echo "Usage: $0 {start|stop|restart}"
+	exit 1
+esac
+
+exit $?
+

commit ff9ac640809aa7e80910a7ecb2a75bd87bdd4b45

buildroot: use QEMU overlay only when platform is QEMU or QEMUv8
common.mk mistakenly uses the QEMU overlay (which currently contains
only an init script for udhcpc) on all platforms. This is incorrect and
causes problems on RPi3 at least [1].

Fixes: commit 751b35bb0b84 ("qemu: buildroot: fix networking")
Links: [1] https://github.com/OP-TEE/optee_os/issues/2478#issuecomment-432818199
Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

common.mk
@@ -204,8 +204,9 @@ buildroot: optee-os
 	@touch ../out-br/extra.conf
 	@echo "BR2_TARGET_GENERIC_GETTY_PORT=\"$(BUILDROOT_GETTY_PORT)\"" >> \
 		../out-br/extra.conf
-	@echo "BR2_ROOTFS_OVERLAY=\"$(ROOT)/build/br-ext/board/qemu/overlay\"" >> \
-		../out-br/extra.conf
+ifneq (,$(BR2_ROOTFS_OVERLAY))
+	@echo "BR2_ROOTFS_OVERLAY=\"$(BR2_ROOTFS_OVERLAY)\"" >> ../out-br/extra.conf
+endif
 	@echo "BR2_PACKAGE_OPTEE_TEST_CROSS_COMPILE=\"$(CROSS_COMPILE_S_USER)\"" >> \
 		../out-br/extra.conf
 	@echo "BR2_PACKAGE_OPTEE_EXAMPLES_CROSS_COMPILE=\"$(CROSS_COMPILE_S_USER)\"" >> \

qemu.mk
@@ -8,6 +8,8 @@ override COMPILE_NS_KERNEL := 32
 override COMPILE_S_USER    := 32
 override COMPILE_S_KERNEL  := 32
 
+BR2_ROOTFS_OVERLAY = $(ROOT)/build/br-ext/board/qemu/overlay
+
 include common.mk
 
 ################################################################################

qemu_v8.mk
@@ -8,6 +8,8 @@ override COMPILE_NS_KERNEL := 64
 override COMPILE_S_USER    := 64
 override COMPILE_S_KERNEL  := 64
 
+BR2_ROOTFS_OVERLAY = $(ROOT)/build/br-ext/board/qemu/overlay
+
 include common.mk
 
 ################################################################################