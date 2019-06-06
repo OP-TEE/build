e1ece8328f9f6102bda339aef4e8bf46975a67bd
Etienne Carriere <etienne.carriere@linaro.org>
2019-06-06 11:42:07 +0200

    br-ext: Rename OP-TEE packages with _ext suffix
    Rename OP-TEE packages in br-ext with a _ext suffix so that
    they do not conflict with the OP-TEE packages from Buildroot.
    
    This change essentially renames Buildroot configuration switches
    BR2_OPTEE_* to BR2_OPTEE_*_EXT_* without changing the current build
    process.
    
    This change is needed for OP-TEE build environment to synchronize with
    Buildroot GIT version [1] or later in the Buildroot integration tree.
    [1] points where OP-TEE packages were introduced in Buildroot,
    right after 2019.02 is released. This was tested with all Buildroot
    release tags from 2018.11 to 2020.02, and recent 2020.05-rc1.
    
    Link: [1] https://git.buildroot.net/buildroot/commit/?id=da49312af9e0c3e7f6df50e7d6f77e1fa2921b7a
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

---
+++ br-ext/package/optee_os_ext/optee_os_ext.mk
@@ -0,0 +1,18 @@
+OPTEE_OS_EXT_VERSION = 1.0
+OPTEE_OS_EXT_SOURCE = local
+OPTEE_OS_EXT_SITE = $(BR2_PACKAGE_OPTEE_OS_EXT_SITE)
+OPTEE_OS_EXT_SITE_METHOD = local
+OPTEE_OS_EXT_SDK = $(BR2_PACKAGE_OPTEE_OS_EXT_SDK)
+
+define OPTEE_OS_EXT_INSTALL_OPTEE_OS_SHLIBS
+	@mkdir -p $(TARGET_DIR)/lib/optee_armtz && \
+		for f in $(OPTEE_OS_EXT_SDK)/lib/*.ta; do \
+			[ -f "$$f" ] || continue; \
+			$(INSTALL) -v -p  --mode=444 \
+				--target-directory=$(TARGET_DIR)/lib/optee_armtz $$f; \
+		done
+endef
+
+OPTEE_OS_EXT_POST_INSTALL_TARGET_HOOKS += OPTEE_OS_EXT_INSTALL_OPTEE_OS_SHLIBS
+
+$(eval $(cmake-package))
