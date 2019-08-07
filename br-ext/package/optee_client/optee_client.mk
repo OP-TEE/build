OP-TEE/build

commit 93758cc1841002b39970cc14989f3eef65f01608

buildroot: provide CFG_TEE_BENCHMARK in optee_client
Provide proper value to optee_client cmake option
CFG_TEE_BENCHMARK

Acked-by: Joakim Bech <joakim.bech@linaro.org>
Signed-off-by: Igor Opaniuk <igor.opaniuk@linaro.org>

br-ext/package/optee_client/optee_client.mk
@@ -4,6 +4,10 @@ OPTEE_CLIENT_SITE = $(BR2_PACKAGE_OPTEE_CLIENT_SITE)
 OPTEE_CLIENT_SITE_METHOD = local
 OPTEE_CLIENT_INSTALL_STAGING = YES
 
+ifeq ($(BR2_PACKAGE_OPTEE_BENCHMARK),y)
+OPTEE_CLIENT_CONF_OPTS = -DCFG_TEE_BENCHMARK=ON
+endif
+
 define OPTEE_CLIENT_INSTALL_SUPPLICANT_SCRIPT
 	$(INSTALL) -m 0755 -D $(OPTEE_CLIENT_PKGDIR)/S30optee \
 		$(TARGET_DIR)/etc/init.d/S30optee

commit 22b9c3b3a78cff96841075574b26ed27315df112

br-ext: optee_client: fix typo
Fix typo for setting CFG_TEE_BENCHMARK in OPTEE_CLIENT_CONF_OPTS.

Fixes: 93758cc1 ("buildroot: provide CFG_TEE_BENCHMARK in optee_client")
Signed-off-by: Victor Chong <victor.chong@linaro.org>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

br-ext/package/optee_client/optee_client.mk
@@ -5,7 +5,7 @@ OPTEE_CLIENT_SITE_METHOD = local
 OPTEE_CLIENT_INSTALL_STAGING = YES
 
 ifeq ($(BR2_PACKAGE_OPTEE_BENCHMARK),y)
-OPTEE_CLIENT_CONF_OPTS = -DCFG_TEE_BENCHMARK=ON
+OPTEE_CLIENT_CONF_OPTS = -DCFG_TEE_BENCHMARK=y
 endif
 
 OPTEE_CLIENT_CONF_OPTS += -DCFG_TA_TEST_PATH=y