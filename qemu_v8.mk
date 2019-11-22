OP-TEE/build

commit c54f4f51fafbd181cc5cea056c0eb46eb1295ef1

qemu, qemu_v8: do not build Linux modules
The QEMU and QEMUv8 builds do not need any kernel module to boot and
run xtest etc. so we can save build time by only building the kernel
image by default.

Signed-off-by: Jerome Forissier <jerome@forissier.org>
Reviewed-by: Joakim Bech <joakim.bech@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

qemu_v8.mk
@@ -142,7 +142,7 @@ LINUX_DEFCONFIG_COMMON_FILES := \
 
 linux-defconfig: $(LINUX_PATH)/.config
 
-LINUX_COMMON_FLAGS += ARCH=arm64
+LINUX_COMMON_FLAGS += ARCH=arm64 zImage
 
 linux: linux-common
 	mkdir -p $(BINARIES_PATH)

qemu.mk
@@ -118,7 +118,7 @@ LINUX_DEFCONFIG_COMMON_FILES := \
 
 linux-defconfig: $(LINUX_PATH)/.config
 
-LINUX_COMMON_FLAGS += ARCH=arm
+LINUX_COMMON_FLAGS += ARCH=arm zImage
 
 linux: linux-common
 	mkdir -p $(BINARIES_PATH)

commit ff4aaae5a6c5278c7f2243e2bb1801167dda7930

qemu_v8: fix linux build
The kernel image for QEMUv8 is Image, not zImage (there is no
compressed image support in Linux for arm64). Fix the makefile.

Fixes: c54f4f51fafb ("qemu, qemu_v8: do not build Linux modules")
Signed-off-by: Jerome Forissier <jerome@forissier.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

qemu_v8.mk
@@ -142,7 +142,7 @@ LINUX_DEFCONFIG_COMMON_FILES := \
 
 linux-defconfig: $(LINUX_PATH)/.config
 
-LINUX_COMMON_FLAGS += ARCH=arm64 zImage
+LINUX_COMMON_FLAGS += ARCH=arm64 Image
 
 linux: linux-common
 	mkdir -p $(BINARIES_PATH)