d77eb5e806d606e7108ca20a13fbd62f77492552
Etienne Carriere <etienne.carriere@linaro.org>
2021-01-22 15:09:21 +0100

    br-ext: install OP-TEE devkit TAs
    Install all TAs found in devkit into target filesystem. This change
    allows OP-TEE test environment to exercise in-tree TAs as the pkcs11 TA.
    
    Acked-by: Jerome Forissier <jerome@forissier.org>
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- br-ext/package/optee_os_ext/optee_os_ext.mk
+++ br-ext/package/optee_os_ext/optee_os_ext.mk
@@ -4,7 +4,7 @@ OPTEE_OS_EXT_SITE = $(BR2_PACKAGE_OPTEE_OS_EXT_SITE)
 OPTEE_OS_EXT_SITE_METHOD = local
 OPTEE_OS_EXT_SDK = $(BR2_PACKAGE_OPTEE_OS_EXT_SDK)
 
-define OPTEE_OS_EXT_INSTALL_OPTEE_OS_SHLIBS
+define OPTEE_OS_EXT_INSTALL_DEVKIT_SHLIBS
 	@mkdir -p $(TARGET_DIR)/lib/optee_armtz && \
 		for f in $(OPTEE_OS_EXT_SDK)/lib/*.ta; do \
 			[ -f "$$f" ] || continue; \
@@ -13,6 +13,19 @@ define OPTEE_OS_EXT_INSTALL_OPTEE_OS_SHLIBS
 		done
 endef
 
-OPTEE_OS_EXT_POST_INSTALL_TARGET_HOOKS += OPTEE_OS_EXT_INSTALL_OPTEE_OS_SHLIBS
+define OPTEE_OS_EXT_INSTALL_DEVKIT_TAS
+	for f in $(OPTEE_OS_EXT_SDK)/ta/*.ta; do \
+		[ -f "$$f" ] || continue; \
+		$(INSTALL) -v -p  --mode=444 \
+			--target-directory=$(TARGET_DIR)/lib/optee_armtz $$f; \
+	done
+endef
+
+define OPTEE_OS_EXT_DO_INSTALL
+	$(OPTEE_OS_EXT_INSTALL_DEVKIT_SHLIBS)
+	$(OPTEE_OS_EXT_INSTALL_DEVKIT_TAS)
+endef
+
+OPTEE_OS_EXT_POST_INSTALL_TARGET_HOOKS += OPTEE_OS_EXT_DO_INSTALL
 
 $(eval $(cmake-package))
