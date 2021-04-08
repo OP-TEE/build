bff6197abd0719dd232ccf668fdaf4447eadeb12
Etienne Carriere <etienne.carriere@linaro.org>
2021-04-08 12:38:39 +0200

    common: remove deprecated dependency for optee-os-clean-common
    Remove deprecated dependencies on target optee-os-clean-common:
    benchmark-app-clean-common, xtest-clean-common and
    optee-examples-clean-common where removed by commit [1].
    
    Fixes build failure with trace like:
      bash> make clean -j$(nproc)
      (...)
      make: *** No rule to make target 'xtest-clean-common', needed by 'optee-os-clean-common'.  Stop.
    
    Link: [1] 819066fb88a6 ("common.mk: remove unused targets and definitions")
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Reviewed-by: Jerome Forissier <jerome@forissier.org>

--- common.mk
+++ common.mk
@@ -448,8 +448,5 @@ optee-os-common:
 	$(MAKE) -C $(OPTEE_OS_PATH) $(OPTEE_OS_COMMON_FLAGS)
 
 .PHONY: optee-os-clean-common
-ifeq ($(CFG_TEE_BENCHMARK),y)
-optee-os-clean-common: benchmark-app-clean-common
-endif
-optee-os-clean-common: xtest-clean-common optee-examples-clean-common
+optee-os-clean-common:
 	$(MAKE) -C $(OPTEE_OS_PATH) $(OPTEE_OS_COMMON_FLAGS) clean
