4e47eeafe93e533968ae171915c17a27feb14437
Jerome Forissier <jerome@forissier.org>
2021-08-11 17:39:20 +0200

    qemu, qemu_v8: set msize when mounting host filesystem
    When VIRTFS_AUTOMOUNT=y and/or PSS_AUTOMOUNT=y, QEMU prints the
    following warning:
    
     qemu-system-aarch64: warning: 9p: degraded performance: a reasonable
     high msize should be chosen on client/guest side (chosen msize is <=
     8192). See https://wiki.qemu.org/Documentation/9psetup#msize for
     details.
    
    Let's set the buffer size to 64K mainly to silence this warning. The
    performance is noticeably increased too although it doesn't really
    matters for typical OP-TEE use cases.
    
    Signed-off-by: Jerome Forissier <jerome@forissier.org>
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

--- br-ext/board/qemu/post-build.sh
+++ br-ext/board/qemu/post-build.sh
@@ -31,13 +31,13 @@ fi
 
 if [[ $VIRTFS_AUTOMOUNT == "y" ]]; then
     grep host "$TARGETDIR"/etc/fstab > /dev/null || \
-    echo "host $VIRTFS_MOUNTPOINT 9p trans=virtio,version=9p2000.L,rw 0 0" >> "$TARGETDIR"/etc/fstab
+    echo "host $VIRTFS_MOUNTPOINT 9p trans=virtio,version=9p2000.L,msize=65536,rw 0 0" >> "$TARGETDIR"/etc/fstab
     echo "[+] shared directory mount added to fstab"
 fi
 
 if [[ $PSS_AUTOMOUNT == "y" ]]; then
     mkdir -p "$TARGETDIR"/data/tee
     grep secure "$TARGETDIR"/etc/fstab > /dev/null || \
-    echo "secure /data/tee 9p trans=virtio,version=9p2000.L,rw 0 0" >> "$TARGET_DIR"/etc/fstab
+    echo "secure /data/tee 9p trans=virtio,version=9p2000.L,msize=65536,rw 0 0" >> "$TARGET_DIR"/etc/fstab
     echo "[+] persistent secure storage mount added to fstab"
 fi
