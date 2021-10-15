d22f2dc9f0004b8c9cf71e46c815aeea1804fef9
Rong Fan <fanrong03@baidu.com>
2021-10-14 19:11:44 -0700

    optee_rust_examples_ext.mk: fix linker not found error
    Following error occurs when running make buildroot QEMU_RUST_ENABLE=y:
    
    $ make buildroot QEMU_RUST_ENABLE=y
    ...
    error: linker  not found
      |
      = note: No such file or directory (os error 2)
    
    error: aborting due to previous error
    
    The toolchains path is not add to the PATH. Fix it.
    
    Signed-off-by: Rong Fan <fanrong03@baidu.com>
    Acked-by: Joakim Bech <joakim.bech@linaro.org>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>
    Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

--- br-ext/package/optee_rust_examples_ext/optee_rust_examples_ext.mk
+++ br-ext/package/optee_rust_examples_ext/optee_rust_examples_ext.mk
@@ -4,6 +4,10 @@ OPTEE_RUST_EXAMPLES_EXT_SITE = $(BR2_PACKAGE_OPTEE_RUST_EXAMPLES_EXT_SITE)
 OPTEE_RUST_EXAMPLES_EXT_SITE_METHOD = local
 OPTEE_RUST_EXAMPLES_EXT_INSTALL_STAGING = YES
 
+ifneq (,$(BR2_PACKAGE_OPTEE_RUST_EXAMPLES_TC_PATH_ENV))
+OPTEE_RUST_EXAMPLES_TC_PATH_ENV = PATH=$(BR2_PACKAGE_OPTEE_RUST_EXAMPLES_TC_PATH_ENV)
+endif
+
 EXAMPLE = $(wildcard examples/*)
 
 HOST_TARGET := aarch64-unknown-linux-gnu
@@ -15,14 +19,13 @@ export OPTEE_DIR = $(@D)/../../..
 export OPTEE_OS_DIR = $(OPTEE_DIR)/optee_os
 export OPTEE_CLIENT_DIR = $(OPTEE_DIR)/out-br/build/optee_client_ext-1.0
 export OPTEE_CLIENT_INCLUDE = $(OPTEE_CLIENT_DIR)/out/export/usr/include
-export PATH := $(PATH):$(OPTEE_DIR)/toolchains/aarch64/bin
 export VENDOR = qemu_v8.mk
 export OPTEE_OS_INCLUDE = $(OPTEE_DIR)/optee_os/out/arm/export-ta_arm64/include
 
 define OPTEE_RUST_EXAMPLES_EXT_BUILD_CMDS
 	@$(foreach f,$(wildcard $(@D)/examples/*/Makefile), \
 		echo Building $f && \
-		$(MAKE) -C $(dir $f) &&) true
+		$(OPTEE_RUST_EXAMPLES_TC_PATH_ENV) $(MAKE) -C $(dir $f) &&) true
 endef
 
 define OPTEE_RUST_EXAMPLES_EXT_INSTALL_TARGET_CMDS
