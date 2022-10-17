1240b4c8cd9dda4ac10bf918e03510901b817f15
Etienne Carriere <etienne.carriere@linaro.org>
2022-10-17 11:30:37 +0200

    br-ext: optee_test_ext: enumerate build targets via shell instead of make
    Fixes [1] by restoring change merge in [1] but removed afterward by [2]
    to overcome Makefile variable max size issue when build environment uses
    large build directive. This change enforces dependencies list defined by
    OPTEE_TEST_EXT_TAS are built first before any in-tree components (libs
    or TAs) is build, making the dependencies list being likely built twice.
    
    The same change is also made on OPTEE_TEST_EXT_INSTALL_TAS despite [2]
    did not consider this install sequence.
    
    Link: [1] https://github.com/OP-TEE/build/issues/597
    Link: [2] https://github.com/OP-TEE/build/pull/393/commits/70f5c8bcc90662719233e22f97044327db3bcb57
    Link: [3] https://github.com/OP-TEE/build/commit/e1ece8328f9f6102bda339aef4e8bf46975a67bd
    Reported-by: Markus S. Wamser <markus.wamser@mixed-mode.de>
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- br-ext/package/optee_test_ext/optee_test_ext.mk
+++ br-ext/package/optee_test_ext/optee_test_ext.mk
@@ -7,7 +7,7 @@ OPTEE_TEST_EXT_DEPENDENCIES = optee_client_ext openssl host-python3-pycryptodome
 OPTEE_TEST_EXT_SDK = $(BR2_PACKAGE_OPTEE_TEST_EXT_SDK)
 OPTEE_TEST_EXT_CONF_OPTS = -DOPTEE_TEST_SDK=$(OPTEE_TEST_EXT_SDK)
 # os_test has dependencies, this enforces a valid build order
-OPTEE_TEST_EXT_TAS = os_test_lib os_test_lib_dl os_test *
+OPTEE_TEST_EXT_TAS = os_test_lib os_test_lib_dl os_test
 uniq = $(if $1,$(firstword $1) $(call uniq,$(filter-out $(firstword $1),$1)))
 
 ifneq ($(BR2_PACKAGE_OPTEE_TEST_EXT_GP_PACKAGE),"")
@@ -29,20 +29,23 @@ define OPTEE_TEST_EXT_PREPARE_GP_SUITE
 endef
 
 define OPTEE_TEST_EXT_BUILD_TAS
-	@$(foreach f,$(call uniq,$(foreach t,$(OPTEE_TEST_EXT_TAS),$(wildcard $(@D)/ta/$(t)/Makefile))), \
-		echo Building $f && \
-			$(MAKE) CROSS_COMPILE="$(shell echo $(BR2_PACKAGE_OPTEE_TEST_EXT_CROSS_COMPILE))" \
-			O=out TA_DEV_KIT_DIR=$(OPTEE_TEST_EXT_SDK) \
-			PYTHON3=$(HOST_DIR)/bin/python3 \
-			$(TARGET_CONFIGURE_OPTS) -C $(dir $f) all &&) true
+	@unset mdeps; for d in $(OPTEE_TEST_EXT_TAS); do mdeps=$$mdeps\ $(@D)/ta/$$d/Makefile; done && \
+	 for f in $$mdeps $(@D)/ta/*/Makefile; \
+	 do \
+	  $(MAKE) CROSS_COMPILE="$(shell echo $(BR2_PACKAGE_OPTEE_TEST_EXT_CROSS_COMPILE))" \
+	    O=out TA_DEV_KIT_DIR=$(OPTEE_TEST_EXT_SDK) \
+	    PYTHON3=$(HOST_DIR)/bin/python3 \
+	    $(TARGET_CONFIGURE_OPTS) -C $${f%/*} all; \
+	done
 endef
 
 define OPTEE_TEST_EXT_INSTALL_TAS
-	@$(foreach f,$(wildcard $(@D)/ta/*/out/*.ta), \
-		mkdir -p $(TARGET_DIR)/lib/optee_armtz && \
-		$(INSTALL) -v -p  --mode=444 \
-			--target-directory=$(TARGET_DIR)/lib/optee_armtz $f \
-			&&) true
+	@for f in $(@D)/ta/*/out/*.ta; \
+	do \
+	  mkdir -p $(TARGET_DIR)/lib/optee_armtz && \
+	  $(INSTALL) -v -p  --mode=444 \
+	    --target-directory=$(TARGET_DIR)/lib/optee_armtz $$f; \
+	done
 endef
 
 
