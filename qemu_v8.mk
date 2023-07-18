OP-TEE/build

commit d217c656517d4e1b3634775807464b709b250553

qemu_v8: remove ENABLE_SME_FOR_*=1 from TF_A_FLAGS
With commit 337ff4f1dd66 ("fix(qemu): enable SVE and SME") included in
TF-A v2.8.0 ENABLE_SME_FOR_NS and ENABLE_SME_FOR_SWD are already
assigned 1 by default. So remove the redundant configuration.

Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

qemu_v8.mk
@@ -162,8 +162,6 @@ TF_A_FLAGS ?= \
 	QEMU_USE_GIC_DRIVER=$(TFA_GIC_DRIVER) \
 	ENABLE_SVE_FOR_NS=1 \
 	ENABLE_SVE_FOR_SWD=1 \
-	ENABLE_SME_FOR_NS=1 \
-	ENABLE_SME_FOR_SWD=1 \
 	BL32_RAM_LOCATION=tdram \
 	DEBUG=$(TF_A_DEBUG) \
 	LOG_LEVEL=$(TF_A_LOGLVL)

commit 10d4f8aef841127bbfd82e9785bfdaea6a7717f3

qemu_v8: restore ENABLE_SME_FOR_SWD=1 in TF_A_FLAGS
ENABLE_SME_FOR_SWD=1 was previously removed on the faulty assumption
that 337ff4f1dd66 ("fix(qemu): enable SVE and SME") was from now on
setting ENABLE_SME_FOR_SWD=1 by default. That's not correct, so restore
ENABLE_SME_FOR_SWD=1 in TF_A_FLAGS.

Fixes: d217c656517d ("qemu_v8: remove ENABLE_SME_FOR_*=1 from TF_A_FLAGS")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

qemu_v8.mk
@@ -170,6 +170,7 @@ TF_A_FLAGS ?= \
 	QEMU_USE_GIC_DRIVER=$(TFA_GIC_DRIVER) \
 	ENABLE_SVE_FOR_NS=1 \
 	ENABLE_SVE_FOR_SWD=1 \
+	ENABLE_SME_FOR_SWD=1 \
 	ENABLE_FEAT_FGT=2 \
 	BL32_RAM_LOCATION=tdram \
 	DEBUG=$(TF_A_DEBUG) \