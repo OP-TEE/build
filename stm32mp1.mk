OP-TEE/build

commit 0509e68cdf6846b9d5846f5171250fd657df18df

stm32mp1: allow to define alternative u-boot device tree
Signed-off-by: Johann Neuhauser <jneuhauser@dh-electronics.com>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

stm32mp1.mk
@@ -29,6 +29,8 @@ else
 $(error Unknown PLATFORM $(PLATFORM))
 endif
 
+STM32MP1_DTS_U_BOOT ?= $(STM32MP1_DTS_BASENAME)
+
 ################################################################################
 # Binary images names
 ################################################################################
@@ -122,7 +124,7 @@ U_BOOT_EXPORTS ?= CROSS_COMPILE="$(CCACHE)$(AARCH32_CROSS_COMPILE)"
 
 u-boot:
 	$(U_BOOT_EXPORTS) $(MAKE) -C $(U_BOOT_PATH) stm32mp15_defconfig
-	$(U_BOOT_EXPORTS) $(MAKE) -C $(U_BOOT_PATH) DEVICE_TREE=$(STM32MP1_DTS_BASENAME) all
+	$(U_BOOT_EXPORTS) $(MAKE) -C $(U_BOOT_PATH) DEVICE_TREE=$(STM32MP1_DTS_U_BOOT) all
 	@$(call install_in_binaries,$(U_BOOT_PATH)/$(U_BOOT_BIN))
 	@$(call install_in_binaries,$(U_BOOT_PATH)/$(U_BOOT_DTB))
 

commit ea195bc68ad6169f0a22d18ba1f1df4dbee757a9

stm32mp1: Fix U-Boot dts file selection for DHCOM devices
U-Boot builds can have different device tree filenames than Linux because,
for example, stm32mp15-based DHCOM devices use a single dts file like
stm32mp15xx-dhcom-pdk2 for all different flavors.

Thats why commit 0509e68cdf68 introduced STM32MP1_DTS_U_BOOT.
Sadly commit 4b263e2f4c3a reverted the usage for the U-Boot build.

So make use of STM32MP1_DTS_U_BOOT again for the U-Boot build.

Fixes: 4b263e2f4c3a ("stm32mp1: build 135F-DK flavor")
Signed-off-by: Johann Neuhauser <jneuhauser@dh-electronics.com>
Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

stm32mp1.mk
@@ -142,7 +142,7 @@ U_BOOT_EXPORTS ?= CROSS_COMPILE="$(CCACHE)$(AARCH32_CROSS_COMPILE)"
 
 u-boot:
 	$(U_BOOT_EXPORTS) $(MAKE) -C $(U_BOOT_PATH) $(STM32MP1_DEFCONFIG_U_BOOT)
-	$(U_BOOT_EXPORTS) $(MAKE) -C $(U_BOOT_PATH) DEVICE_TREE=$(STM32MP1_DTS_BASENAME) all
+	$(U_BOOT_EXPORTS) $(MAKE) -C $(U_BOOT_PATH) DEVICE_TREE=$(STM32MP1_DTS_U_BOOT) all
 	@$(call install_in_binaries,$(U_BOOT_PATH)/$(U_BOOT_BIN))
 	@$(call install_in_binaries,$(U_BOOT_PATH)/$(U_BOOT_DTB))
 