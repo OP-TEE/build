OP-TEE/build

commit e43efa5b691a5f594db982134d4116ad1e625412

stm32mp1: define STM32MP157A-DHCOR-AVENGER96
Signed-off-by: Johann Neuhauser <jneuhauser@dh-electronics.com>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

stm32mp1.mk
@@ -16,6 +16,10 @@ include common.mk
 ifeq ($(PLATFORM),stm32mp1-157A_DK1)
 BREXT_FLAVOR		= STM32MP157A-DK1
 STM32MP1_DTS_BASENAME	= stm32mp157a-dk1
+else ifeq ($(PLATFORM),stm32mp1-157A_DHCOR_AVENGER96)
+BREXT_FLAVOR		= STM32MP157A-DHCOR-AVENGER96
+STM32MP1_DTS_BASENAME	= stm32mp157a-dhcor-avenger96
+STM32MP1_DTS_U_BOOT		= stm32mp15xx-dhcor-avenger96
 else ifeq ($(PLATFORM),stm32mp1-157C_DK2)
 BREXT_FLAVOR		= STM32MP157C-DK2
 STM32MP1_DTS_BASENAME	= stm32mp157c-dk2

br-ext/board/stmicroelectronics/stm32mp1-tz/genimage-STM32MP157A-DHCOR-AVENGER96.cfg
@@ -1,0 +1,34 @@
+image sdcard.img {
+	hdimage {
+		gpt = "true"
+	}
+
+	partition fsbl {
+		image = "tf-a-stm32mp157a-dhcor-avenger96.stm32"
+		size = 256K
+	}
+
+	partition fip {
+		image = "fip.bin"
+		partition-type-uuid = L
+		size = 4M
+	}
+
+	partition u-boot-env {
+		partition-type-uuid = L
+		size = 4M
+	}
+
+	partition bootfs {
+		image = "bootfs.ext2"
+		partition-type-uuid = L
+		size = 32M
+		bootable = "yes"
+	}
+
+	partition rootfs {
+		image = "rootfs.ext2"
+		partition-type-uuid = L
+		size = 128M
+	}
+}

br-ext/board/stmicroelectronics/stm32mp1-tz/overlay-STM32MP157A-DHCOR-AVENGER96/boot/extlinux/extlinux.conf
@@ -1,0 +1,6 @@
+TIMEOUT 20
+DEFAULT stm32mp15-buildroot
+LABEL stm32mp15-buildroot
+  kernel /boot/uImage
+  devicetree /boot/stm32mp157a-dhcor-avenger96.dtb
+  append root=/dev/mmcblk0p5 rootwait rw console=ttySTM0,115200

commit 137ad304d2300e8067bb6ad9866303ad5c94ad11

stm32mp1: fix indentation of U-Boot base name config
Fixes indentation of DHCOR Avenger96 and DHCOM PDK2 boards' U-Boot DTS
file base name configuration.

Fixes: e43efa5b691a ("stm32mp1: define STM32MP157A-DHCOR-AVENGER96")
Fixes: 5914639cb690 ("stm32mp1: define STM32MP157C-DHCOM-PDK2")
Reviewed-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

stm32mp1.mk
@@ -21,11 +21,11 @@ STM32MP1_DTS_U_BOOT 	?= $(STM32MP1_DTS_BASENAME)-scmi
 else ifeq ($(PLATFORM),stm32mp1-157A_DHCOR_AVENGER96)
 BREXT_FLAVOR		= STM32MP157A-DHCOR-AVENGER96
 STM32MP1_DTS_BASENAME	= stm32mp157a-dhcor-avenger96
-STM32MP1_DTS_U_BOOT		= stm32mp15xx-dhcor-avenger96
+STM32MP1_DTS_U_BOOT	= stm32mp15xx-dhcor-avenger96
 else ifeq ($(PLATFORM),stm32mp1-157C_DHCOM_PDK2)
 BREXT_FLAVOR		= STM32MP157C-DHCOM-PDK2
 STM32MP1_DTS_BASENAME	= stm32mp157c-dhcom-pdk2
-STM32MP1_DTS_U_BOOT		= stm32mp15xx-dhcom-pdk2
+STM32MP1_DTS_U_BOOT	= stm32mp15xx-dhcom-pdk2
 else ifeq ($(PLATFORM),stm32mp1-157C_DK2)
 BREXT_FLAVOR		= STM32MP157C-DK2
 STM32MP1_DTS_BASENAME	= stm32mp157c-dk2