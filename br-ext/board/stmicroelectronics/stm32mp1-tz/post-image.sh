5433020c70ace0d8e60fb31a14478553f04bdda4
Etienne Carriere <etienne.carriere@foss.st.com>
2023-12-12 14:55:15 +0100

    stm32mp1: generate extlinux.conf at build time
    Generate extlinux config file, used by U-Boot to driver default boot
    sequences, a from template where only the Linux DTB file name is set
    from target platform specific name.
    
    Acked-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
    Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

--- br-ext/board/stmicroelectronics/stm32mp1-tz/post-image.sh
+++ br-ext/board/stmicroelectronics/stm32mp1-tz/post-image.sh
@@ -15,6 +15,7 @@ echo "- genimage config file: ${2}"
 echo "- external bins path:   ${3}"
 echo "- bootfs overlay path:  ${4}"
 echo "- TF-A DTB basename:    ${5}"
+echo "- Linux DTB basename:   ${6}"
 
 [ ! -z "${2}" -a -e ${2} ] || die "Error: missing argument genimage config file"
 [ ! -z "${3}" -a -d ${3} ] || die "Error: missing argument external binaries dir"
@@ -40,6 +41,11 @@ done
 echo "[GEN] Generate genimage file for target ${5} GPT formating"
 sed s/TFA_BOARD_REV/${5}/g ${2} > ${GENIMAGE_IN}
 
+# Set Linux DTB file basename in genimage config file
+echo "[GEN] Generate extlinux.conf for target ${6} autoboot"
+sed -i s/LINUX_DTB_BASENAME/${6}/g ${BOOTFS_DIR}/boot/extlinux/extlinux.conf
+
+# Create bootfs EXT2 file system.
 mkfs.ext2 -L bootfs -d ${BOOTFS_DIR} ${BINARIES_DIR}/bootfs.ext2 32M || exit 1
 
 # Generate image from generated partition images and genimage config file
