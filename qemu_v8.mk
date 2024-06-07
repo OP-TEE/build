OP-TEE/build

commit f68f33e845958f7cf3b49de030b60b6f9536d0c6

qemu_v8: enable max CPU for Xen
Enable the QEMU max CPU for Xen. With this change simplify the
configuration a little.

Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>

qemu_v8.mk
@@ -489,27 +489,29 @@ run: all
 
 
 ifeq ($(XEN_BOOT),y)
-QEMU_CPU	?= cortex-a57
-QEMU_MEM 	?= 3072
-QEMU_SMP	?= 4
 QEMU_VIRT	= true
-QEMU_XEN	?= -drive if=none,file=$(XEN_EXT4),format=raw,id=hd1 \
-		   -device virtio-blk-device,drive=hd1
-else
-ifeq ($(SPMC_AT_EL),2)
+else ifeq ($(SPMC_AT_EL),2)
 QEMU_VIRT	= true
 else
 QEMU_VIRT	= false
 endif
+
+ifeq ($(XEN_BOOT),y)
+QEMU_MEM 	?= 3072
+QEMU_SMP	?= 4
+QEMU_XEN	?= -drive if=none,file=$(XEN_EXT4),format=raw,id=hd1 \
+		   -device virtio-blk-device,drive=hd1
+else
+QEMU_SMP 	?= 2
+QEMU_MEM 	?= 1057
+endif
+
 ifeq ($(SPMC_AT_EL),n)
 QEMU_SME	= on
 else
 QEMU_SME	= off
 endif
 QEMU_CPU	?= max,sme=$(QEMU_SME),pauth-impdef=on
-QEMU_SMP 	?= 2
-QEMU_MEM 	?= 1057
-endif
 
 ifeq ($(MEMTAG),y)
 QEMU_MTE	= on

commit f3c2ae92844a9b17b7bdf3b7f8514d4204f21ec5

qemu_v8: fix non-FF-A Xen configuration
Xen doesn't work with SME enabled. For SPMC_AT_EL == 1, 2, or 3 SME is
already disabled when starting QEMU, but not for SPMC_AT_EL=n and
XEN_BOOT=y, so fix this with another check.

Fixes: f68f33e84595 ("qemu_v8: enable max CPU for Xen")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Tested-by: Jerome Forissier <jerome.forissier@linaro.org> (vexpress-qemu_armv8a)

qemu_v8.mk
@@ -506,7 +506,9 @@ QEMU_SMP 	?= 2
 QEMU_MEM 	?= 1057
 endif
 
-ifeq ($(SPMC_AT_EL),n)
+ifeq ($(XEN_BOOT),y)
+QEMU_SME	= off
+else ifeq ($(SPMC_AT_EL),n)
 QEMU_SME	= on
 else
 QEMU_SME	= off