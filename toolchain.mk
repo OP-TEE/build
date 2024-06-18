e8ee821d49d9d52a900284a61c9b386db5abf4d0
Jerome Forissier <jerome.forissier@linaro.org>
2024-06-18 13:38:50 +0200

    toolchain.mk: add clang-toolchains-build to build Clang from sources
    Add a makefile target to download and build the Clang 18.1.7 sources and
    install the compiler into <PRJ_ROOT>/toolchains/clang-18.1.7/bin.
    
    Usage example:
    
     $ make clang-toolchains-build
     $ PATH=$(pwd)/../toolchains/clang-18.1.7/bin:$PATH \
         make -j$(nproc) COMPILER=clang check
    
    The Clang build takes some significant time (about 12 minutes on my
    machine, a 24-core Intel Core i9-12900K) so we may want to store a
    pre-built package somewhere in the future and have "make
    clang-toolchains" download that package. In the mean time, this
    new target allows to test OP-TEE with a recent LLVM toolchain.
    
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

--- toolchain.mk
+++ toolchain.mk
@@ -158,3 +158,16 @@ $(AARCH64_PATH)/.done:
 $(AARCH32_PATH)/.done:
 	$(call build_toolchain,aarch32,$(AARCH32_PATH),arm,gnueabihf)
 endif
+
+# Recipe to build Clang from sources and install it in
+# $(TOOLCHAIN_ROOT)/clang-x.y.z/bin
+
+CLANG_BUILD_VER_MAJ=18
+CLANG_BUILD_VER=$(CLANG_BUILD_VER_MAJ).1.7
+CLANG_BUILD_IMAGE=clang-$(CLANG_BUILD_VER)-$(UNAME_M)
+
+clang-toolchains-build:
+	docker build --build-arg VER_MAJ=$(CLANG_BUILD_VER_MAJ) --build-arg VER=$(CLANG_BUILD_VER) -t $(CLANG_BUILD_IMAGE) clang/
+	id=$$(docker create $(CLANG_BUILD_IMAGE)) && \
+		docker cp $${id}:/root/clang-$(CLANG_BUILD_VER) $(TOOLCHAIN_ROOT) && \
+		docker rm $${id}
