OP-TEE/build

commit 329573518db6a601e5c202a66a2ae44f6e983cda

qemu: support new path to QEMU v9.2.0
Add support for the new build path used for QEMU v9.2.0. The makefile
will search first the new location and then the old location to maintain
backward compatibility.

Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>

qemu.mk
@@ -170,6 +170,10 @@ QEMU_RUN_ARGS = $(QEMU_BASE_ARGS)
 QEMU_RUN_ARGS += $(QEMU_RUN_ARGS_COMMON)
 QEMU_RUN_ARGS += -s -S -serial tcp:127.0.0.1:$(QEMU_NW_PORT) -serial tcp:127.0.0.1:$(QEMU_SW_PORT)
 
+# The aarch64-softmmu part of the path to qemu-system-aarch64 was removed
+# somewhere between 8.1.2 and 9.1.2
+QEMU_BIN = $(or $(wildcard $(QEMU_BUILD)/qemu-system-aarch64),$(wildcard $(QEMU_BUILD)/aarch64-softmmu/qemu-system-aarch64),qemu-system-aarch64-not-found)
+
 .PHONY: run-only
 run-only:
 	ln -sf $(ROOT)/out-br/images/rootfs.cpio.gz $(BINARIES_PATH)/
@@ -178,8 +182,7 @@ run-only:
 	$(call launch-terminal,$(QEMU_NW_PORT),"Normal World")
 	$(call launch-terminal,$(QEMU_SW_PORT),"Secure World")
 	$(call wait-for-ports,$(QEMU_NW_PORT),$(QEMU_SW_PORT))
-	cd $(BINARIES_PATH) && $(QEMU_BUILD)/arm-softmmu/qemu-system-arm \
-		$(QEMU_RUN_ARGS)
+	cd $(BINARIES_PATH) && $(QEMU_BIN) $(QEMU_RUN_ARGS)
 
 ifneq ($(filter check,$(MAKECMDGOALS)),)
 CHECK_DEPS := all
@@ -203,7 +206,7 @@ QEMU_CHECK_ARGS += -serial stdio -serial file:serial1.log
 check: $(CHECK_DEPS)
 	ln -sf $(ROOT)/out-br/images/rootfs.cpio.gz $(BINARIES_PATH)/
 	cd $(BINARIES_PATH) && \
-		export QEMU=$(QEMU_BUILD)/arm-softmmu/qemu-system-arm && \
+		export QEMU=$(QEMU_BIN) && \
 		export QEMU_CHECK_ARGS="$(QEMU_CHECK_ARGS)" && \
 		export XEN_BOOT=n && \
 		expect $(ROOT)/build/qemu-check.exp -- $(check-args) || \

commit d60852624b79ccd436003ad4eafa711139e22f3a

qemu: fix path to QEMU
The name of the QEMU v7 binary is qemu-system-arm not qemu-system-aarch64,
so fix it.

Fixes: 329573518db6 ("qemu: support new path to QEMU v9.2.0")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Acked-by: Etienne Carriere <etienne.carriere@foss.st.com>
Reviewed-by: Jerome Forissier <jerome.forissier@linaro.org>
Tested-by: Jerome Forissier <jerome.forissier@linaro.org>

qemu.mk
@@ -170,9 +170,9 @@ QEMU_RUN_ARGS = $(QEMU_BASE_ARGS)
 QEMU_RUN_ARGS += $(QEMU_RUN_ARGS_COMMON)
 QEMU_RUN_ARGS += -s -S -serial tcp:127.0.0.1:$(QEMU_NW_PORT) -serial tcp:127.0.0.1:$(QEMU_SW_PORT)
 
-# The aarch64-softmmu part of the path to qemu-system-aarch64 was removed
+# The arm-softmmu part of the path to qemu-system-arm was removed
 # somewhere between 8.1.2 and 9.1.2
-QEMU_BIN = $(or $(wildcard $(QEMU_BUILD)/qemu-system-aarch64),$(wildcard $(QEMU_BUILD)/aarch64-softmmu/qemu-system-aarch64),qemu-system-aarch64-not-found)
+QEMU_BIN = $(or $(wildcard $(QEMU_BUILD)/qemu-system-arm),$(wildcard $(QEMU_BUILD)/arm-softmmu/qemu-system-arm),qemu-system-arm-not-found)
 
 .PHONY: run-only
 run-only: