d3d4b47e842fcf637584f640bedce00e57a7515b
Joachim Foerster <joachim.foerster@missinglinkelectronics.com>
2025-12-12 19:39:16 +0100

    versal: Enhance boot_jtag.sh and versal.tcl
    - Use xsdb instead of xsct.
    
    - Offer option -i to enter interactive mode after initialing loading of
      BOOT.BIN.
    
    - Offer option -u to specify hw_server URL (tcp:<host>:<port>) of an
      already running hw_server instance, instead of starting a new, local
      hw_server (when executing "connect" without "-url ...").
    
    - Advertise custom commands in interactive mode.
    
    Signed-off-by: Joachim Foerster <joachim.foerster@missinglinkelectronics.com>
    Reviewed-by: Nathan Menhorn <nathan.menhorn@amd.com>
    Tested-by: Nathan Menhorn <nathan.menhorn@amd.com>
    Reviewed-by: Lucas Kiker <lucas.kiker@amd.com>
    Reviewed-by: Sindhu Raveendra <Sindhu.Raveendra@amd.com>
    Reviewed-by: Clemens Nasenberg <clemens.nasenberg@amd.com>

--- versal/boot_jtag.sh
+++ versal/boot_jtag.sh
@@ -1,7 +1,63 @@
 #!/bin/bash
 
-read -p "Enter full path to the Petalinux install [i.e: /opt/petalinux.2022.1]: " path
+# autodetect xsdb availability
+detect_petalinux_xsdb()
+{
+	if [ -n $PETALINUX_VER ]; then
+		# if Petalinux > v2023.2
+		if [ -e $PETALINUX/components/xsct/bin/xsdb ]; then
+			XSDB=$PETALINUX/components/xsct/bin/xsdb
 
-. "${path}"/settings.sh
+		# if Petalinux > v2019.1
+		else
+			XSDB=$PETALINUX/tools/xsct/bin/xsdb
+		fi
+	fi
+}
 
-xsct ./versal.tcl
+if [ -z "$XSDB" ]; then
+	# if within Vitis environment, xsdb is in PATH
+	if which xsdb >/dev/null; then
+		XSDB=xsdb
+
+	# if within Petalinux environment, xsdb is NOT in PATH
+	else
+		detect_petalinux_xsdb
+	fi
+fi
+
+# fallback to querying user for Petalinux installation unless xsdb has been
+# found
+if [ -z "$XSDB" ]; then
+	read -p "Enter full path to the Petalinux install [i.e: /opt/petalinux.2022.1]: " path
+	. "${path}"/settings.sh
+
+	# if within Petalinux environment, xsdb is NOT in PATH
+	detect_petalinux_xsdb
+fi
+
+opts=
+tclargs=
+while [ $# -gt 0 ]; do
+	case $1 in
+	-i|-interactive)
+		opts="$opts -interactive"
+		tclargs="$tclargs -interactive"
+		;;
+	-u|-url)
+		tclargs="$tclargs -url $2"
+		shift 1
+		;;
+	-h|-help|--help)
+		echo "usage: $0 [-i|-interactive] [-u|-url <hw_server-url>]"
+		echo ""
+		echo "options:"
+		echo "	-i		enter xsdb interactive mode after initial loading of BOOT.BIN"
+		echo "	-u <url>	hw_server URL to use when executing xsdb command \"connect\""
+		exit 0
+		;;
+	esac
+	shift 1
+done
+
+env -C $(dirname $0) $XSDB $opts versal.tcl $tclargs
--- versal/versal.tcl
+++ versal/versal.tcl
@@ -1,6 +1,80 @@
-connect
-after 1000
-target 1
-rst
-targets -set -nocase -filter {name =~ "*Versal*"}
-device program "./BOOT.BIN"
+set interactive 0
+set hw_server_url ""
+set hw_server_url_opt 0
+foreach arg $argv {
+	if {$hw_server_url_opt} {
+		set hw_server_url_opt 0
+		set hw_server_url $arg
+		continue
+	}
+	if {"$arg" eq "-interactive"} {
+		set interactive 1
+	} elseif {"$arg" eq "-url"} {
+		set hw_server_url_opt 1
+	}
+}
+
+proc _connect {} {
+	global hw_server_url
+
+	if {[string length $hw_server_url] > 0} {
+		puts "info: using hw_server URL from command line argument: \"$hw_server_url\""
+		connect -url $hw_server_url
+	} elseif {[info exists ::env(HW_SERVER_URL)] &&
+		  [string length $::env(HW_SERVER_URL)]} {
+		puts "info: using hw_server URL from environment variable HW_SERVER_URL: \"$::env(HW_SERVER_URL)\""
+		connect -url $::env(HW_SERVER_URL)
+	} else {
+		puts "info: launching local hw_server instance"
+		connect
+	}
+	after 1000
+}
+
+proc _reset {} {
+	puts "Selecting target Versal"
+	target -set -filter {name =~ "Versal *"}
+
+	# PMC_MULTI_BOOT (PMC_GLOBAL) Register
+	#   0x00F1110004
+	puts "Clear PMC_MULTI_BOOT register (0x00f1110004 = 0x0)"
+	mwr 0x00f1110004 0x0
+
+	# BOOT_MODE_USER (CRP) Register
+	#   0x00f1260200[15:12] == 0x0  => JTAG boot mode
+	#   0x00f1260200[8]     == 0x1  => use [15:12] as boot mode
+	set boot_mode_user_reg 0x0100
+	puts "Switch to JTAG boot mode, BOOT_MODE_USER register (0x00f1260200 = [format 0x%04x $boot_mode_user_reg])"
+	mwr 0x00f1260200 $boot_mode_user_reg
+
+	puts "Selecting target PMC"
+	target -set -filter {name =~ "*PMC*"}
+
+	puts "Reset system"
+	rst -type system
+}
+
+proc _program {} {
+	_reset
+
+	puts "Selecting target Versal"
+	target -set -filter {name =~ "Versal *"}
+
+	puts "Loading BOOT.BIN"
+	device program "BOOT.BIN"
+
+	puts "PLM memory log after loading BOOT.BIN:"
+	plm log
+}
+
+_connect
+_program
+
+if {$interactive} {
+	puts "info: Staying interactive; available custom commands:"
+	puts ""
+	puts "	_program	(re-)program BOOT.BIN"
+	puts "	_reset		switch to JTAG boot mode and reset"
+	puts "	_connect	(re-)connect to target"
+	puts ""
+}
